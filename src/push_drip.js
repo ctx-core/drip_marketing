1bca3d2b28eed265eacc784375bf7a4b846cc71b
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/push_drip.js
+++
@@ -1,8 +0,0 @@
-// declare const window
-export function push_drip(...arg_a) {
-    if (typeof window._dcq !== 'undefined') {
-        window._dcq.push(...arg_a);
-    }
-}
-export { push_drip as push__drip };
-//# sourceMappingURL=src/push_drip.js.map
